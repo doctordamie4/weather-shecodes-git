bad333acdd75398dfb11663a9ed379a6700fe978
doctordamie4 <67023315+doctordamie4@users.noreply.github.com>
2020-08-08 22:40:56 -0700

    added celsius and fahrenheit functions and added city to api URL

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ function search(city) {
   let place = document.querySelector ("#place-city");
 place.innerHTML =`${searchInput.value}`;
   let apiKey = "1d4b68593b5ef58c6ebeb70b9aa9976d";
-  let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&units=metric&appid=${apiKey}`;
+  let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
   axios.get(`${apiUrl}`).then(showTemperature);
  
 }
@@ -29,6 +29,21 @@ function handleSubmit(event) {
   let place = document.querySelector ("#place-city");
 place.innerHTML =`${searchInput.value}`;
 }
+
+function displayFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#grade");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML= Math.round(fahrenheitTemperature);
+}
+
+function displayCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#grade");
+  temperatureElement.innerHTML= Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
